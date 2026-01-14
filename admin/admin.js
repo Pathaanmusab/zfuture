9d9742d63cc2fed72baac0a79450275b9470516f
Md Musab khan <mdmusab009@gmail.com>
2026-01-14 18:38:20 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,43 +1,103 @@
 let products = JSON.parse(localStorage.getItem("products")||"[]");
 let categories = JSON.parse(localStorage.getItem("categories")||"[]");
 
-function addProduct(){
-  let price = +priceInput.value;
-  let offer = +offerInput.value;
-  let finalPrice = price - (price*offer/100);
-
-  let product = {
-    id: Date.now(),
-    name: name.value,
-    price,
-    offer,
-    finalPrice,
-    category: category.value,
-    desc: desc.value,
-    images: [img1.value, img2.value, img3.value].filter(x=>x),
-    return: return.value,
-    returnDays: returnDays.value,
-    warranty: warranty.value,
-    warrantyTime: warrantyTime.value
-  };
-
-  products.push(product);
-  localStorage.setItem("products", JSON.stringify(products));
-
-  if(!categories.includes(product.category)){
-    categories.push(product.category);
-    localStorage.setItem("categories", JSON.stringify(categories));
-  }
-
-  alert("Product Added");
-  location.reload();
+/* CATEGORY */
+function addCategory(){
+ let c=document.getElementById("catName").value.trim();
+ if(!c)return;
+ categories.push(c);
+ localStorage.setItem("categories",JSON.stringify(categories));
+ document.getElementById("catName").value="";
+ renderCats();
+}
+function renderCats(){
+ let box=document.getElementById("catList");
+ box.innerHTML="";
+ categories.forEach((c,i)=>{
+  box.innerHTML+=`
+  <div class="item">
+   ${c}
+   <button onclick="delCat(${i})">Delete</button>
+  </div>`;
+ });
+}
+function delCat(i){
+ categories.splice(i,1);
+ localStorage.setItem("categories",JSON.stringify(categories));
+ renderCats();
 }
 
-let list=document.getElementById("list");
-products.forEach(p=>{
-  list.innerHTML+=`
-    <div class="card">
-      <b>${p.name}</b><br>
-      ₹${p.finalPrice} (${p.offer}% OFF)
-    </div>`;
-});
+/* PRODUCT */
+function saveProduct(){
+ let id=document.getElementById("pid").value||Date.now();
+ let name=pname.value;
+ let cat=pcat.value;
+ let price=Number(pprice.value);
+ let offer=Number(poffer.value)||0;
+ let finalPrice=price - (price*offer/100);
+
+ let images=pimages.value.split("\n").filter(x=>x);
+
+ let obj={
+  id,name,category:cat,price,offer,finalPrice,
+  desc:pdesc.value,
+  images,
+  return:preturn.value,
+  returnDays:preturnDays.value,
+  warranty:pwarranty.value,
+  warrantyTime:pwarrantyTime.value
+ };
+
+ let i=products.findIndex(x=>x.id==id);
+ if(i>-1) products[i]=obj;
+ else products.push(obj);
+
+ localStorage.setItem("products",JSON.stringify(products));
+ clearForm();
+ renderProducts();
+}
+
+function renderProducts(){
+ let box=document.getElementById("plist");
+ box.innerHTML="";
+ products.forEach(p=>{
+  box.innerHTML+=`
+  <div class="item">
+   <b>${p.name}</b> (${p.category})<br>
+   ₹${p.finalPrice}
+   <div class="actions">
+    <button onclick="editProduct('${p.id}')">Edit</button>
+    <button onclick="delProduct('${p.id}')">Delete</button>
+   </div>
+  </div>`;
+ });
+}
+
+function editProduct(id){
+ let p=products.find(x=>x.id==id);
+ pid.value=p.id;
+ pname.value=p.name;
+ pcat.value=p.category;
+ pprice.value=p.price;
+ poffer.value=p.offer;
+ pdesc.value=p.desc;
+ pimages.value=p.images.join("\n");
+ preturn.value=p.return;
+ preturnDays.value=p.returnDays;
+ pwarranty.value=p.warranty;
+ pwarrantyTime.value=p.warrantyTime;
+}
+
+function delProduct(id){
+ products=products.filter(x=>x.id!=id);
+ localStorage.setItem("products",JSON.stringify(products));
+ renderProducts();
+}
+
+function clearForm(){
+ document.querySelectorAll("input,textarea").forEach(i=>i.value="");
+}
+
+/* INIT */
+renderCats();
+renderProducts();
