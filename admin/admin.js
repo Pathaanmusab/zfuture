2c31eaece93db54a813932305a759d6427a43274
Md Musab khan <mdmusab009@gmail.com>
2026-01-14 15:20:07 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,73 +1,48 @@
-let categories = JSON.parse(localStorage.getItem("categories")) || [];
 let products = JSON.parse(localStorage.getItem("products")) || [];
+let slider = JSON.parse(localStorage.getItem("slider")) || [];
 
-const catList = document.getElementById("catList");
-const pCategory = document.getElementById("pCategory");
-const productList = document.getElementById("productList");
-
-function save(){
-  localStorage.setItem("categories", JSON.stringify(categories));
-  localStorage.setItem("products", JSON.stringify(products));
-}
-
-/* CATEGORY */
-function addCategory(){
-  let name = catName.value.trim();
-  if(!name) return;
-  categories.push(name);
-  catName.value="";
-  save();
-  renderCategories();
-}
-
-function renderCategories(){
-  catList.innerHTML="";
-  pCategory.innerHTML="";
-  categories.forEach((c,i)=>{
-    catList.innerHTML += `<li>${c}</li>`;
-    pCategory.innerHTML += `<option>${c}</option>`;
+function addSlider(){
+  slider.push({
+    url: surl.value,
+    type: stype.value
   });
+  localStorage.setItem("slider",JSON.stringify(slider));
+  alert("Slider Added");
 }
 
-/* PRODUCT */
 function addProduct(){
-  let p = {
-    id: Date.now(),
-    name: pName.value,
-    price: pPrice.value,
-    category: pCategory.value,
-    images: pImages.value.split(",").map(i=>i.trim()),
-    warranty: pWarranty.value,
-    return: pReturn.value
-  };
-  products.push(p);
-  save();
-  renderProducts();
-}
-
-function deleteProduct(id){
-  products = products.filter(p=>p.id!==id);
-  save();
-  renderProducts();
-}
-
-function renderProducts(){
-  productList.innerHTML="";
-  products.forEach(p=>{
-    productList.innerHTML += `
-      <div class="product">
-        <b>${p.name}</b> – ₹${p.price}
-        <small>Category: ${p.category}</small>
-        <small>Warranty: ${p.warranty}</small>
-        <small>Return: ${p.return}</small>
-        <small>Images: ${p.images.length}</small>
-        <div class="actions">
-          <button onclick="deleteProduct(${p.id})">Delete</button>
-        </div>
-      </div>
-    `;
+  products.push({
+    name:name.value,
+    price:+price.value,
+    offer:+offer.value,
+    images:images.value.split(","),
+    category:category.value,
+    warranty:warranty.value,
+    return:returnp.value,
+    hide:false
   });
+  localStorage.setItem("products",JSON.stringify(products));
+  location.reload();
 }
 
-renderCategories();
-renderProducts();
+const list = document.getElementById("list");
+products.forEach((p,i)=>{
+  list.innerHTML += `
+  <div class="card">
+    <b>${p.name}</b><br>
+    <button onclick="del(${i})">Delete</button>
+    <button onclick="hide(${i})">${p.hide?'Show':'Hide'}</button>
+  </div>`;
+});
+
+function del(i){
+  products.splice(i,1);
+  localStorage.setItem("products",JSON.stringify(products));
+  location.reload();
+}
+
+function hide(i){
+  products[i].hide = !products[i].hide;
+  localStorage.setItem("products",JSON.stringify(products));
+  location.reload();
+}
