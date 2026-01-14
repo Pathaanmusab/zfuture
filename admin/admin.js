58f5f7b58c1915774355096ec670f74aa664112d
Md Musab khan <mdmusab009@gmail.com>
2026-01-14 18:27:04 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,83 +1,43 @@
-let categories = JSON.parse(localStorage.getItem("categories")||"[]");
 let products = JSON.parse(localStorage.getItem("products")||"[]");
+let categories = JSON.parse(localStorage.getItem("categories")||"[]");
 
-function save(){
-  localStorage.setItem("categories",JSON.stringify(categories));
-  localStorage.setItem("products",JSON.stringify(products));
-}
-
-function addCategory(){
-  let c = catName.value.trim();
-  if(!c) return;
-  categories.push(c);
-  save();
-  catName.value="";
-  showCats();
-}
-
-function showCats(){
-  catList.innerHTML="";
-  categories.forEach(c=>{
-    catList.innerHTML+=`<div>${c}</div>`;
-  });
-}
-showCats();
-
-/* PRODUCT */
 function addProduct(){
-  let price = Number(pPrice.value);
-  let offer = Number(pOffer.value)||0;
-  let final = price - (price*offer/100);
+  let price = +priceInput.value;
+  let offer = +offerInput.value;
+  let finalPrice = price - (price*offer/100);
 
-  let obj={
-    id:Date.now().toString(),
-    name:pName.value,
-    price:price,
-    offer:offer,
-    finalPrice:Math.round(final),
-    category:pCategory.value,
-    desc:pDesc.value,
-    warranty:pWarranty.value,
-    return:pReturn.value,
-    images:pImages.value.split(",")
+  let product = {
+    id: Date.now(),
+    name: name.value,
+    price,
+    offer,
+    finalPrice,
+    category: category.value,
+    desc: desc.value,
+    images: [img1.value, img2.value, img3.value].filter(x=>x),
+    return: return.value,
+    returnDays: returnDays.value,
+    warranty: warranty.value,
+    warrantyTime: warrantyTime.value
   };
 
-  products.push(obj);
-  save();
-  showProducts();
+  products.push(product);
+  localStorage.setItem("products", JSON.stringify(products));
 
-  pName.value="";
-  pPrice.value="";
-  pOffer.value="";
-  pCategory.value="";
-  pDesc.value="";
-  pWarranty.value="";
-  pReturn.value="";
-  pImages.value="";
+  if(!categories.includes(product.category)){
+    categories.push(product.category);
+    localStorage.setItem("categories", JSON.stringify(categories));
+  }
+
+  alert("Product Added");
+  location.reload();
 }
 
-function showProducts(){
-  productList.innerHTML="";
-  products.forEach(p=>{
-    productList.innerHTML+=`
-    <div class="product">
-      <b>${p.name}</b>
-      <div class="small">${p.category}</div>
-      <div>₹${p.finalPrice}</div>
-      <button onclick="view('${p.id}')">View</button>
-      <button onclick="del('${p.id}')">Delete</button>
+let list=document.getElementById("list");
+products.forEach(p=>{
+  list.innerHTML+=`
+    <div class="card">
+      <b>${p.name}</b><br>
+      ₹${p.finalPrice} (${p.offer}% OFF)
     </div>`;
-  });
-}
-showProducts();
-
-function del(id){
-  products = products.filter(p=>p.id!==id);
-  save();
-  showProducts();
-}
-
-function view(id){
-  localStorage.setItem("viewProduct",id);
-  window.open("../product.html","_blank");
-}
+});
